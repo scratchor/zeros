23538de1df7bedc60321c65df119fadbf8d9e53a
Владислав Царапкин <scratchor@mail.ru>
2018-09-25 22:41:04 +0300

    task 7 ready

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 module.exports = function getZerosCount(number) {
   // your implementation
-}
+
+  function calc(num) {
+    let res = 0;
+    while(num >= 5) {
+        num/=5;
+        res+=Math.floor(num);
+    }
+    return res;
+};
+  return calc(number);
+};
